c708685e02984a1a5852e2fe9106b5a344654595
Panyue Wang <pywang@ucdavis.edu>
2024-06-22 16:55:37 -0700

    fixed hidden layer back propagation

--- neural_network.py
+++ neural_network.py
@@ -19,11 +19,11 @@ def relu_derivative(x) -> float:
 
 
 def our_function(x) -> float:
-    output = np.zeros(x.size)
-    output[x > 0.1] = 1
+    output = np.power(x, 3) + 2 * np.power(x, 2) + 10
     return output
-# np.power(x, 3) + 2 * np.power(x, 2) + 10
 
+    # output = np.zeros(x.size)
+    # output[x > 0.1] = 1
 
 def softmax(xi, xj):
     return np.exp(xi) / np.sum(np.exp(xj))
@@ -34,7 +34,9 @@ class hidden_layer:
         # weights are randomly assigned
         self.weights = np.random.rand(num_of_data, input_size)
         # biases are usually initialized to be 0
-        self.biases = np.zeros([input_size, 1])
+        self.biases = np.zeros([num_of_data, input_size])
+        print("hidden layer weights: ", self.weights)
+        print("hidden layer biases: ", self.biases)
 
     def forward(self, inputs):
         self.X = np.dot(self.weights, inputs) + self.biases
@@ -54,40 +56,40 @@ class hidden_layer:
                relu_derivative(self.X)
 
     def backward(self, dSSR, output_layer_weights, learning_rate):
-        step_size_b = self.dSSRdb(dSSR, output_layer_weights) * learning_rate
-        self.biases -= step_size_b
-        step_size_w = self.dSSRdw(dSSR, output_layer_weights) * learning_rate
-        self.weights -= step_size_w
+        step_size_b = np.sum(self.dSSRdb(dSSR, output_layer_weights), axis=1) * learning_rate
+        self.biases -= step_size_b.reshape(self.biases.shape)
+        step_size_w = np.sum(self.dSSRdw(dSSR, output_layer_weights), axis=1) * learning_rate
+        self.weights -= step_size_w.reshape(self.weights.shape)
 
 
 class output_layer:
     def __init__(self, hidden_size, output_size):
         self.weights = np.random.rand(output_size, hidden_size)
-        self.biases = np.zeros([output_size, 1])
+        self.biases = np.zeros(output_size)
+        print("output layer weights: ", self.weights)
+        print("output layer biases: ", self.biases)
 
     def forward(self, inputs):
         self.X = np.dot(self.weights, inputs) + self.biases
         self.inputs = inputs
-        self.output = relu(self.X)
+        self.output = self.X
         return self.output
 
-    def dSSRdw(self, dSSR_times_db):
-        return dSSR_times_db * self.output
-
-    def dSSRdb(self, observed):
-        return np.sum(-2 * (observed - self.inputs))
+    def dSSRdw(self, dSSR):
+        return dSSR * self.inputs
 
     def backward(self, dSSR, learning_rate):
-        step_size_b = dSSR * learning_rate
+        step_size_b = np.sum(dSSR) * learning_rate
         self.biases -= step_size_b
-        step_size_w = self.dSSRdw(dSSR) * learning_rate
+        # updating the weights of each neuron
+        step_size_w = np.sum(self.dSSRdw(dSSR), axis=1) * learning_rate
         self.weights -= step_size_w
         return self.weights
 
 
 # Define the NeuralNetwork class
 class NeuralNetwork:
-    def __init__(self, input_size, hidden_size, output_size,
+    def __init__(self, input_size, num_of_data, hidden_size, output_size,
                  learning_rate=0.01):
         self.learning_rate = learning_rate
         self.hidden_layer = hidden_layer(input_size, hidden_size)
@@ -99,7 +101,7 @@ class NeuralNetwork:
         return output_layer_output
 
     def dSSR(self, expected, predicted):
-        return np.sum(-2 * (expected - predicted))
+        return -2 * (expected - predicted)
 
     def train(self, inputs, expected_output, epochs=100):
         for epoch in range(epochs):
@@ -122,49 +124,49 @@ class NeuralNetwork:
 
 if __name__ == "__main__":
     # Number of inputs
-    input_size = 10
+    input_size = 1
 
     # Output size
     output_size = 1
 
     # Size of hidden layer
-    hidden_size = 10
+    hidden_size = 2
 
     # Number of data
-    num_of_data = 1
+    num_of_data = 10
 
     # Generate random input data
     inputs = np.random.rand(num_of_data * input_size)
 
     # test output
-    y = 14.2
-    # y = our_function(inputs).reshape([input_size, num_of_data])
+    # y = np.random.rand(num_of_data)
+    y = our_function(inputs).reshape([input_size, num_of_data])
 
-    nn = NeuralNetwork(input_size, hidden_size, output_size)
+    nn = NeuralNetwork(input_size, num_of_data, hidden_size, output_size)
 
-    nn.train(inputs.reshape([input_size, num_of_data]), y, epochs=200)
+    nn.train(inputs.reshape([input_size, num_of_data]), y, epochs=100)
 
     outputs = nn.forward(inputs.reshape([input_size, num_of_data]))
     print("shape of outputs:", outputs.shape)
-    test_inputs = np.random.rand(num_of_data * input_size)
-    test_outputs = our_function(test_inputs)
-    predicted_outputs = nn.forward(test_inputs.reshape([input_size, num_of_data]))
+    validation_inputs = np.random.rand(num_of_data * input_size)
+    validation_outputs = our_function(validation_inputs)
+    predicted_outputs = nn.forward(validation_inputs.reshape([input_size, num_of_data]))
     print("original inputs:\n", inputs)
     print("expected output:\n", y)
     print("trained output:\n", outputs)
-    print("test inputs:\n", test_inputs)
-    print("test outputs:\n", test_outputs)
+    print("validation inputs:\n", validation_inputs)
+    print("validation outputs:\n", validation_outputs)
     print("trained output with test input:\n", predicted_outputs)
-    # fig, ax = plt.subplots(4, 1, figsize=(12, 15))
-    # ax[0].plot(inputs, y)
-    # ax[0].set_xlabel("original function")
+    fig, ax = plt.subplots(4, 1, figsize=(12, 15))
+    ax[0].scatter(inputs, y)
+    ax[0].set_xlabel("original function")
 
-    # ax[1].scatter(inputs, outputs.flatten())
-    # ax[1].set_xlabel("trained function")
+    ax[1].scatter(inputs, outputs.flatten())
+    ax[1].set_xlabel("trained function")
 
-    # ax[2].scatter(test_inputs, test_outputs)
-    # ax[2].set_xlabel("original function using different input")
+    ax[2].scatter(validation_inputs, validation_outputs)
+    ax[2].set_xlabel("original function using different input")
 
-    # ax[3].scatter(test_inputs, predicted_outputs)
-    # ax[3].set_xlabel("trained function using different input")
-    # plt.show()
+    ax[3].scatter(validation_inputs, predicted_outputs)
+    ax[3].set_xlabel("trained function using different input")
+    plt.show()
