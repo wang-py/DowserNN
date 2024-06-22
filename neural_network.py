0da061aac5239774f2d9fd958761da9ded5e74f2
Panyue Wang <pywang@ucdavis.edu>
2024-06-22 10:02:34 -0700

    fixed naming

--- neural_network.py
+++ neural_network.py
@@ -18,51 +18,80 @@ def relu_derivative(x) -> float:
     return np.where(x > 0, 1, 0)
 
 
-def dSSR(observed, predicted):
-    return np.sum(-2 * (observed - predicted))
-
-
 def our_function(x) -> float:
     return np.power(x, 3) + 2 * np.power(x, 2) + 10
 
 
-class input_layer:
+def softmax(xi, xj):
+    return np.exp(xi) / np.sum(np.exp(xj))
+
+
+class hidden_layer:
     def __init__(self, input_size, num_of_data):
+        # weights are randomly assigned
         self.weights = np.random.rand(num_of_data, input_size)
-        self.biases = np.random.rand(input_size, 1)
+        # biases are usually initialized to be 0
+        self.biases = np.zeros([input_size, 1])
 
     def forward(self, inputs):
         self.outputs = relu(np.dot(self.weights, inputs) + self.biases)
         return self.outputs
 
+    def dSSR(observed, predicted):
+        return np.sum(-2 * (observed - predicted))
 
-class hidden_layer:
+    def dSSRdw(self, dSSR_times_db):
+        return dSSR_times_db * self.output
+
+    def dSSRdb(self, observed):
+        return np.sum(-2 * (observed - self.output))
+
+    def backward(self, expected_output, learning_rate):
+        dSSR_times_db = self.dSSRdb(expected_output)
+        step_size_b = dSSR_times_db * learning_rate
+        self.biases -= step_size_b
+        step_size_w = self.dSSRdw(dSSR_times_db) * learning_rate
+        self.weights -= np.dot(self.weights, step_size_w)
+
+
+class output_layer:
     def __init__(self, hidden_size, output_size):
         self.weights = np.random.rand(output_size, hidden_size)
-        self.biases = np.random.rand(hidden_size, 1)
+        self.biases = np.zeros([output_size, 1])
 
     def forward(self, inputs):
         self.inputs = inputs
         self.output = relu(np.dot(self.weights, inputs) + self.biases)
         return self.output
 
+    def dSSR(observed, predicted):
+        return np.sum(-2 * (observed - predicted))
+
+    def dSSRdw(self, dSSR_times_db):
+        return dSSR_times_db * self.output
+
+    def dSSRdb(self, observed):
+        return np.sum(-2 * (observed - self.output))
+
     def backward(self, expected_output, learning_rate):
-        step_size = dSSR(expected_output, self.output) * learning_rate
-        self.biases -= step_size * self.biases
-        self.weights -= step_size * self.weights
+        dSSR_times_db = self.dSSRdb(expected_output)
+        step_size_b = dSSR_times_db * learning_rate
+        self.biases -= step_size_b
+        step_size_w = self.dSSRdw(dSSR_times_db) * learning_rate
+        self.weights -= np.dot(self.weights, step_size_w)
 
 
 # Define the NeuralNetwork class
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size, learning_rate=0.01):
         self.learning_rate = learning_rate
-        self.input_layer = input_layer(input_size, hidden_size)
-        self.hidden_layer = hidden_layer(hidden_size, output_size)
+        self.hidden_layer = hidden_layer(input_size, hidden_size)
+        self.output_layer = output_layer(hidden_size, output_size)
 
     def forward(self, inputs):
-        input_layer_output = self.input_layer.forward(inputs)
-        hidden_layer_output = self.hidden_layer.forward(input_layer_output)
-        return hidden_layer_output
+        hidden_layer_output = self.hidden_layer.forward(inputs)
+        output_layer_output = self.output_layer.forward(hidden_layer_output)
+        return output_layer_output
 
     def train(self, inputs, expected_output, epochs=1000):
         for epoch in range(epochs):
@@ -70,9 +99,8 @@ class NeuralNetwork:
             actual_output = self.forward(inputs)
 
             # Backward pass
-            self.hidden_layer.backward(
-                expected_output, self.learning_rate
-            )
+            self.output_layer.backward(expected_output, self.learning_rate)
+            self.hidden_layer.backward(expected_output, self.learning_rate)
 
             # Calculate loss
             loss = np.mean((expected_output - actual_output) ** 2)
@@ -85,33 +113,34 @@ if __name__ == "__main__":
     input_size = 10
 
     # Output size
-    output_size = 1
+    output_size = 2
 
     # Size of hidden layer
     hidden_size = 10
 
     # Number of data
-    num_of_data = 100
+    num_of_data = 3
 
     # Generate random input data
     inputs = np.random.rand(num_of_data * input_size)
 
     # test output
-    y = our_function(inputs).reshape([input_size, hidden_size])
+    y = our_function(inputs).reshape([input_size, num_of_data])
 
     nn = NeuralNetwork(input_size, hidden_size, output_size)
 
     nn.train(inputs.reshape([input_size, num_of_data]), y, epochs=2000)
 
-    outputs = nn.forward(inputs)
-    test_inputs = np.random.rand(input_size)
+    outputs = nn.forward(inputs.reshape([input_size, num_of_data]))
+    print("shape of outputs:", outputs.shape)
+    test_inputs = np.random.rand(num_of_data * input_size)
     test_outputs = our_function(test_inputs)
-    predicted_outputs = nn.forward(test_inputs)
+    predicted_outputs = nn.forward(test_inputs.reshape([input_size, num_of_data]))
     fig, ax = plt.subplots(4, 1, figsize=(12, 15))
     ax[0].scatter(inputs, y)
     ax[0].set_xlabel("original function")
 
-    ax[1].scatter(inputs, outputs)
+    ax[1].scatter(inputs, outputs.flatten())
     ax[1].set_xlabel("trained function")
 
     ax[2].scatter(test_inputs, test_outputs)
