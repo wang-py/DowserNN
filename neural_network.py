a2cab979a1680fd1d10901618a7ddf82a114f640
Panyue Wang <pywang@ucdavis.edu>
2024-06-24 19:16:14 -0700

    fixed plotting

--- neural_network.py
+++ neural_network.py
@@ -33,12 +33,12 @@ def generate_input(input_dim, output_dim, num_of_data):
 
 
 def softmax(Z):
-    return np.exp(Z) / np.sum(np.exp(Z), axis=0)
+    return np.exp(Z) / np.sum(np.exp(Z), axis=1).reshape(-1, 1)
 
 
 def forward(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
-    A1 = sigmoid(Z1)
+    A1 = relu(Z1)
     Z2 = W2.dot(A1) + b2
     A2 = relu(Z2)
 
@@ -50,9 +50,9 @@ def calculate_gradient(Z1, A1, Z2, A2, W2, X, dSSR):
     dSSRdW2 = dSSR.dot(A1.T)
     dW2 = dSSRdW2 / N
     db2 = np.sum(dSSR, axis=1) / N
-    dSSRdW1 = (W2.T.dot(dSSR) * sigmoid_derivative(Z1)).dot(X.T)
+    dSSRdW1 = (W2.T.dot(dSSR) * relu_derivative(Z1)).dot(X.T)
     dW1 = dSSRdW1 / N
-    dSSRdb1 = W2.T.dot(dSSR) * sigmoid_derivative(Z1)
+    dSSRdb1 = W2.T.dot(dSSR) * relu_derivative(Z1)
     db1 = np.sum(dSSRdb1, axis=1) / N
 
     return dW1, db1, dW2, db2
@@ -76,10 +76,11 @@ def get_SSR(expected, predicted):
 
 
 def train(W1, b1, W2, b2, X, y, step_size, iters):
+    # y_one_hot_vec = np.ones(y.shape)
     for i in range(iters):
         Z1, A1, Z2, A2 = forward(W1, b1, W2, b2, X)
         SSR = get_SSR(y, A2)
-        if i % 10 == 0:
+        if i % 100 == 0:
             print(f"SSR at {i}: {SSR}")
         dSSR = get_dSSR(y, A2)
         # print(f"dSSR: {dSSR}")
@@ -92,6 +93,10 @@ def train(W1, b1, W2, b2, X, y, step_size, iters):
     return W1, b1, W2, b2, A2
 
 
+def output(argmax_vec, A2):
+    return A2[argmax_vec]
+
+
 if __name__ == "__main__":
     # Number of inputs
     input_dim = 1
@@ -100,19 +105,19 @@ if __name__ == "__main__":
     output_size = 1
 
     # Size of hidden layer
-    hidden_size = 100
+    hidden_size = 200
 
     # Number of data
-    num_of_data = 10
+    num_of_data = 1000
 
     # Generate random input data
-    inputs = np.random.rand(input_dim * num_of_data)
+    inputs = np.random.rand(input_dim * num_of_data) - 0.5
     # initialilze weights
     W1 = np.random.rand(hidden_size, input_dim) / np.sqrt(input_dim)
     b1 = np.zeros([hidden_size, 1])
 
-    W2 = np.random.rand(hidden_size, hidden_size) / np.sqrt(hidden_size)
-    b2 = np.zeros([hidden_size, 1])
+    W2 = np.random.rand(output_size, hidden_size) / np.sqrt(hidden_size)
+    b2 = np.zeros([output_size, 1])
 
     y = our_function(inputs)
     inputs_reshaped = inputs.reshape([input_dim, num_of_data])
@@ -120,20 +125,16 @@ if __name__ == "__main__":
     W1, b1, W2, b2, A2 = train(W1, b1, W2, b2,
                                inputs_reshaped,
                                y_reshaped,
-                               step_size=0.001, iters=2000)
+                               step_size=0.01, iters=5000)
 
     # test output
-    test_X = np.random.rand(num_of_data * input_dim)
-    y = our_function(test_X)
     A2 = forward(W1, b1, W2, b2, inputs.reshape([input_dim, num_of_data]))[3]
-    A2 = np.mean(A2, axis=0)
 
-    validation_inputs = np.random.rand(num_of_data * input_dim)
+    validation_inputs = np.random.rand(num_of_data * input_dim) - 0.5
     validation_outputs = our_function(validation_inputs)
     predicted_outputs = forward(W1, b1, W2, b2, validation_inputs.reshape([input_dim, num_of_data]))[3]
-    predicted_outputs = np.mean(predicted_outputs, axis=0)
-    print("original inputs:\n", inputs)
-    print("expected output:\n", y)
+    print("original inputs:\n", inputs_reshaped)
+    print("expected output:\n", y_reshaped)
     print("trained output:\n", A2)
     print("validation inputs:\n", validation_inputs)
     print("validation outputs:\n", validation_outputs)
