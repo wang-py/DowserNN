aa9d78ac57a6cf261770bbd7032e5586630f7615
Panyue Wang <pywang@ucdavis.edu>
2024-06-26 16:34:16 -0700

    toy model is working

--- neural_network.py
+++ neural_network.py
@@ -33,14 +33,14 @@ def generate_input(input_dim, output_dim, num_of_data):
 
 
 def softmax(Z):
-    return np.exp(Z) / np.sum(np.exp(Z), axis=1).reshape(-1, 1)
+    return np.exp(Z) / np.sum(np.exp(Z), axis=0)
 
 
 def forward(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
     A1 = relu(Z1)
     Z2 = W2.dot(A1) + b2
-    A2 = relu(Z2)
+    A2 = softmax(Z2)
 
     return Z1, A1, Z2, A2
 
@@ -75,12 +75,21 @@ def get_SSR(expected, predicted):
     return np.sum(np.square(expected - predicted))
 
 
+def get_cross_entropy(A2):
+    return np.sum(-np.log(A2))
+
+
 def train(W1, b1, W2, b2, X, y, step_size, iters):
     # y_one_hot_vec = np.ones(y.shape)
     for i in range(iters):
         Z1, A1, Z2, A2 = forward(W1, b1, W2, b2, X)
+        # argmax_index = np.argmax(A2)
+        # output = A2[argmax_index]
+        # print(f"argmax_index: {argmax_index}")
+        cross_entropy = get_cross_entropy(A2)
         SSR = get_SSR(y, A2)
         if i % 100 == 0:
+            print(f"cross entropy at {i}: {cross_entropy}")
             print(f"SSR at {i}: {SSR}")
         dSSR = get_dSSR(y, A2)
         # print(f"dSSR: {dSSR}")
@@ -99,16 +108,16 @@ def output(argmax_vec, A2):
 
 if __name__ == "__main__":
     # Number of inputs
-    input_dim = 1
+    input_dim = 80
 
     # Output size
-    output_size = 1
+    output_size = 2
 
     # Size of hidden layer
-    hidden_size = 128
+    hidden_size = 80
 
     # Number of data
-    num_of_data = 100
+    num_of_data = 6
 
     # Generate random input data
     inputs = np.random.rand(input_dim * num_of_data) - 0.5
@@ -119,35 +128,39 @@ if __name__ == "__main__":
     W2 = np.random.rand(output_size, hidden_size) / np.sqrt(hidden_size)
     b2 = np.zeros([output_size, 1])
 
-    y = our_function(inputs)
+    y = np.array([[0, 1], [0, 1], [1, 0], [1, 0], [1, 0], [1, 0]]).T
     inputs_reshaped = inputs.reshape([input_dim, num_of_data])
-    y_reshaped = y.reshape([input_dim, num_of_data])
+    y_reshaped = y.reshape([-1, 1])
+    np.set_printoptions(precision=3, suppress=True)
     W1, b1, W2, b2, A2 = train(W1, b1, W2, b2,
                                inputs_reshaped,
-                               y_reshaped,
-                               step_size=0.01, iters=50000)
+                               y,
+                               step_size=0.02, iters=4000)
 
     # test output
     A2 = forward(W1, b1, W2, b2, inputs.reshape([input_dim, num_of_data]))[3]
+    # print(f"original inputs: {inputs}")
+    print(f"expected output:\n{y}")
+    print(f"trained output:\n{A2}")
 
-    validation_inputs = np.random.rand(num_of_data * input_dim) - 0.5
-    validation_outputs = our_function(validation_inputs)
-    predicted_outputs = forward(W1, b1, W2, b2, validation_inputs.reshape([input_dim, num_of_data]))[3]
-    print("original inputs:\n", inputs_reshaped)
-    print("expected output:\n", y_reshaped)
-    print("trained output:\n", A2)
-    print("validation inputs:\n", validation_inputs)
-    print("validation outputs:\n", validation_outputs)
-    print("trained output with test input:\n", predicted_outputs)
-    fig, ax = plt.subplots(2, 1, figsize=(12, 10))
-    ax[0].scatter(inputs, y, label="original function", color='b')
-    ax[0].scatter(inputs, A2, label="trained function", color='r')
-    ax[0].legend()
+    # validation_inputs = np.random.rand(num_of_data * input_dim) - 0.5
+    # validation_outputs = our_function(validation_inputs)
+    # predicted_outputs = forward(W1, b1, W2, b2, validation_inputs.reshape([input_dim, num_of_data]))[3]
+    # print("original inputs:\n", inputs_reshaped)
+    # print("expected output:\n", y)
+    # print("trained output:\n", A2)
+    # print("validation inputs:\n", validation_inputs)
+    # print("validation outputs:\n", validation_outputs)
+    # print("trained output with test input:\n", predicted_outputs)
+    # fig, ax = plt.subplots(2, 1, figsize=(12, 10))
+    # ax[0].scatter(inputs, y, label="original function", color='b')
+    # ax[0].scatter(inputs, A2, label="trained function", color='r')
+    # ax[0].legend()
 
-    ax[1].scatter(validation_inputs, validation_outputs, color='b',
-                  label="original function using different input")
-    ax[1].scatter(validation_inputs, predicted_outputs.flatten(), color='r',
-                  label="trained function using different input")
-    ax[1].legend()
+    # ax[1].scatter(validation_inputs, validation_outputs, color='b',
+    #               label="original function using different input")
+    # ax[1].scatter(validation_inputs, predicted_outputs.flatten(), color='r',
+    #               label="trained function using different input")
+    # ax[1].legend()
 
-    plt.show()
+    # plt.show()
