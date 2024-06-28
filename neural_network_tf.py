60be4e4847d848c6f71dfa5162296995b8b33e8a
Panyue Wang <pywang@ucdavis.edu>
2024-06-28 16:40:39 -0700

    tensorflow implementation

--- neural_network_tf.py
+++ neural_network_tf.py
@@ -2,39 +2,49 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
+if __name__ == "__main__":
+    # Generate training and validation data
+    X = np.load("test_data/X.npy")
+    input_dim = X.shape[1]
+    N = X.shape[0]
+    y = np.load("test_data/y.npy")
 
-# Generate training and validation data
-X = np.load("test_data/X.npy")
-input_dim = X.shape[1]
-N = X.shape[0]
-y = np.load("test_data/y.npy")
+    X_data = tf.convert_to_tensor(X)
+    y_data = tf.convert_to_tensor(y)
 
-# Create a neural network model
-model = Sequential()
-model.add(Dense(80, input_shape=(N, input_dim),
-                activation='relu', kernel_initializer='he_normal'))
-model.add(Dense(2, input_shape=(80, 80), activation='softmax'))
+    # Create a neural network model
+    model = Sequential()
+    model.add(
+        Dense(
+            80,
+            activation="relu",
+            kernel_initializer="he_normal",
+        )
+    )
+    model.add(Dense(2, activation="softmax"))
 
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.01), loss='binary_crossentropy')
+    # Compile the model
+    model.compile(optimizer=Adam(learning_rate=0.01), loss="binary_crossentropy")
+    model.build((N, input_dim))
 
-model.summary()
-# Train the model
-history = model.fit(X, y, epochs=800, batch_size=32)
+    model.summary()
+    # Train the model
+    history = model.fit(X, y, epochs=80, batch_size=32)
+    print(history.history.keys())
 
-# Generate new input data for prediction
+    # Generate new input data for prediction
 
-# Plot the results
-# plt.figure(figsize=(12, 6))
-
-# Plot training loss
-# plt.subplot(1, 2, 1)
-# plt.plot(history.history['loss'], label='Training Loss')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.title('Training Loss')
-# plt.legend()
-# 
-# plt.show()
+    # Plot training loss
+    fig, ax = plt.subplots(figsize=(12, 10))
+    ax.plot(history.history['loss'], label='cross entropy')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss')
+    ax.set_title('Training Loss')
+    ax.legend()
+    # ax[1].plot(history.history['cross_entropy'], label='cross entropy')
+    # ax[1].set_title('cross entropy')
+    # ax[1].legend()
+    plt.show()
