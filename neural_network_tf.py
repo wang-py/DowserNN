7c0f606930d1fc8ecf925dc4bd390030eb3064e2
Panyue Wang <pywang@ucdavis.edu>
2024-07-07 22:49:49 -0700

    added weights visualization

--- neural_network_tf.py
+++ neural_network_tf.py
@@ -2,15 +2,28 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras import callbacks
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from matplotlib.animation import ArtistAnimation
+
+
+weights_history = []
+
+
+class weights_visualization_callback(callbacks.Callback):
+    def on_epoch_end(self, batch, logs):
+        weights_1, biases_1, weights_2, biases_2 = model.get_weights()
+        # print('on_epoch_end() model.weights:', weights_1)
+        weights_history.append(weights_1)
+
 
 if __name__ == "__main__":
     # Generate training and validation data
     X = np.load("test_data/CI_X.npy")
     y = np.load("test_data/CI_y.npy")
 
-    training_N = int(274)
+    training_N = int(50000)
     X_data = tf.convert_to_tensor(X[:training_N, :])
     y_data = tf.convert_to_tensor(y[:training_N, :])
     input_dim = X_data.shape[1]
@@ -18,6 +31,8 @@ if __name__ == "__main__":
 
     X_test = tf.convert_to_tensor(X[training_N:, :])
     y_test = tf.convert_to_tensor(y[training_N:, :])
+    # record weights during each training iteration
+    callback = weights_visualization_callback()
     # Create a neural network model
     model = Sequential()
     model.add(
@@ -36,7 +51,8 @@ if __name__ == "__main__":
 
     model.summary()
     # Train the model
-    history = model.fit(X_data, y_data, epochs=64, batch_size=32)
+    history = model.fit(X_data, y_data, epochs=64, batch_size=32,
+                        callbacks=callback)
     y_predicted = model.predict(X_data)
     np.set_printoptions(precision=4, suppress=True)
     # print("expected output:\n", y_data)
@@ -50,12 +66,20 @@ if __name__ == "__main__":
     print(f"loss: {test_loss:.0%}")
 
     # Plot training loss
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.plot(history.history['loss'], label='cross entropy')
-    ax.set_xlabel('Epoch')
-    ax.set_ylabel('Loss')
-    ax.set_title('Training Loss')
-    ax.legend()
+    # fig, ax = plt.subplots(figsize=(8, 6))
+    # ax.plot(history.history['loss'], label='cross entropy')
+    # ax.set_xlabel('Epoch')
+    # ax.set_ylabel('Loss')
+    # ax.set_title('Training Loss')
+    # ax.legend()
+    fig_a, ax_a = plt.subplots()
+    epochs = 64
+    artists = []
+    for i in range(epochs):
+        container = ax_a.imshow(weights_history[i])
+        artists.append([container])
+
+    ani = ArtistAnimation(fig=fig_a, artists=artists, interval=100)
     # ax[1].plot(history.history['cross_entropy'], label='cross entropy')
     # ax[1].set_title('cross entropy')
     # ax[1].legend()
