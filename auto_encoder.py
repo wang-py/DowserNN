59b7e3f7f3540e026c39c403fbf8de137a0ca8ba
Panyue Wang <pywang@ucdavis.edu>
2024-07-29 19:48:40 -0700

    reading pdb data

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,27 +1,44 @@
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.optimizers import Adam
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras import utils
 from training_visualization import weights_visualization_callback
 from training_visualization import weights_history_visualizer
 
+
+seed_val = 1029
+utils.set_random_seed(seed_val)
+
+
 if __name__ == "__main__":
-    # Define the dimensions of the input data
-    input_dim = 784  # 28x28 images
-    encoding_dim = 32  # bottleneck dimension
+    X_file = sys.argv[1]
+    X = np.load(X_file)
+
+    training_N = int(X.shape[0] * 0.8)  # int(33000)
+    X_data = tf.convert_to_tensor(X[:training_N, :])
+    input_dim = X_data.shape[1]
+    hidden_dim = 128
+    encoding_dim = 32
+    N = X_data.shape[0]
+
+    X_test = tf.convert_to_tensor(X[training_N:, :])
 
     # Define the input layer
     input_layer = Input(shape=(input_dim,))
 
     # Define the encoder layers
-    encoded = Dense(128, activation="relu")(input_layer)
-    encoded = Dense(64, activation="relu")(encoded)
+    encoded = Dense(encoding_dim * 4, activation="relu")(input_layer)
+    encoded = Dense(encoding_dim * 2, activation="relu")(encoded)
     encoded = Dense(encoding_dim, activation="relu")(encoded)
 
     # Define the decoder layers
-    decoded = Dense(64, activation="relu")(encoded)
-    decoded = Dense(128, activation="relu")(decoded)
-    decoded = Dense(input_dim, activation="sigmoid")(decoded)
+    decoded = Dense(encoding_dim * 2, activation="relu")(encoded)
+    decoded = Dense(encoding_dim * 4, activation="relu")(decoded)
+    decoded = Dense(input_dim, activation="linear")(decoded)
 
     # callback for visualization
     num_of_layers = 6
@@ -33,37 +50,30 @@ if __name__ == "__main__":
     encoder = Model(inputs=input_layer, outputs=encoded)
 
     # Compile the autoencoder model
-    autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
+    autoencoder.compile(optimizer=Adam(learning_rate=0.001),
+                        loss="mse")
+    autoencoder.build((N, input_dim))
     autoencoder.summary()
 
     # Train the autoencoder model
-    from keras.datasets import mnist
-
-    (x_train, _), (x_test, _) = mnist.load_data()
-
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
-
-    x_train = x_train.reshape((len(x_train), input_dim))
-    x_test = x_test.reshape((len(x_test), input_dim))
 
     history = autoencoder.fit(
-        x_train,
-        x_train,
+        X_data,
+        X_data,
         epochs=50,
         batch_size=256,
         shuffle=True,
-        validation_data=(x_test, x_test),
+        validation_data=(X_test, X_test),
         callbacks=callback
     )
 
     plt.plot(history.history['loss'], label='cross entropy')
     plt.show()
     # Use the encoder to compress the data
-    compressed_data = encoder.predict(x_test)
+    # compressed_data = encoder.predict(x_test)
 
     # Use the autoencoder to reconstruct the data
-    reconstructed_data = autoencoder.predict(x_test)
+    # reconstructed_data = autoencoder.predict(x_test)
     weights_history = callback.get_weights()
     weights_visualizer = weights_history_visualizer(weights_history, mode='2d')
     weights_visualizer.visualize()
