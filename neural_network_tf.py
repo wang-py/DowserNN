c7a7c800f662cb00c660fe961f0d5eadc90d312d
Panyue Wang <pywang@ucdavis.edu>
2024-08-05 15:03:57 -0700

    fine tuning

--- neural_network_tf.py
+++ neural_network_tf.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     X_validate = tf.convert_to_tensor(X_yes)
     y_validate = tf.convert_to_tensor(y_yes)
     input_dim = X_data.shape[1]
-    hidden_dim = 64
+    hidden_dim = 128
     N = X_data.shape[0]
 
     X_test = tf.convert_to_tensor(X[training_N:, :])
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     callback = weights_visualization_callback(num_of_layers)
     model = build_NN(num_of_layers, N, input_dim, hidden_dim,
                      learning_rate=0.001)
-    epochs = 200
+    epochs = 100
     # Train the model
     history = model.fit(X_data, y_data, epochs=epochs, batch_size=32,
                         callbacks=callback)
