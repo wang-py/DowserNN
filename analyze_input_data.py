4f2d16ffd19d1b5cea01083494a8d439c638baae
Panyue Wang <pywang@ucdavis.edu>
2024-09-04 18:36:42 -0700

    pca draft

--- analyze_input_data.py
+++ analyze_input_data.py
@@ -1,6 +1,20 @@
 import numpy as np
-import os
+import sys
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == "__main__":
+    input_data_X = np.load(sys.argv[1])
+    input_data_y = np.load(sys.argv[2])
+
+    # components in input data
+    components = 2
+    pca = PCA(n_components=components)
+    X_scaled = StandardScaler().fit_transform(input_data_X)
+    pca_features = pca.fit_transform(X_scaled)
+
+    plt.scatter(pca_features[:, 0], pca_features[:, 1])
+    plt.show()
+
     pass
