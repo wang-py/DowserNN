a6577940d2c7e14399268f6c242fa9a8fc93684d
Panyue Wang <pywang@ucdavis.edu>
2025-01-15 16:18:12 -0800

    add include radius as an argument

--- categorize_water.py
+++ categorize_water.py
@@ -4,9 +4,10 @@ from pdb_input_processing import read_pdb, find_distances
 parser = argparse.ArgumentParser(prog='categorize_water.py',
                                  description='script that separates water molecules into surface and internal water and output them as pdbs',
                                  )
-parser.add_argument('filename')
-parser.add_argument('surface_file')
-parser.add_argument('surface_water')
+parser.add_argument('-f', '--filename')
+parser.add_argument('-s', '--surface_file')
+parser.add_argument('-o', '--surface_water')
+parser.add_argument('-r', '--include_radius')
 
 
 def read_surface_points(surface_file):
@@ -54,9 +55,12 @@ if __name__ == "__main__":
     print(args.filename)
     print(args.surface_file)
     print(args.surface_water)
+    if not args.include_radius:
+        args.include_radius = 3
+    print(args.include_radius)
     atom_info = read_pdb(args.filename)
     surface_points = read_surface_points(args.surface_file)
     water_data = get_water_data(atom_info)
-    surface_water_data = get_surface_water(water_data, surface_points)
+    surface_water_data = get_surface_water(water_data, surface_points, args.include_radius)
     write_surface_water_to_pdb(surface_water_data, args.surface_water)
     pass
