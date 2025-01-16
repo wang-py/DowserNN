b2d4ceb3711c0fb58fb9a781eb751cb7f9f7f4f9
Panyue Wang <pywang@ucdavis.edu>
2025-01-16 15:01:11 -0800

    added internal water for output

--- categorize_water.py
+++ categorize_water.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(prog='categorize_water.py',
                                  )
 parser.add_argument('-f', '--filename')
 parser.add_argument('-s', '--surface_file')
-parser.add_argument('-o', '--surface_water')
+parser.add_argument('-o', '--output_filename')
 parser.add_argument('-r', '--include_radius')
 
 
@@ -31,21 +31,24 @@ def get_water_data(atom_info):
     return water_data
 
 
-def get_surface_water(water_data, surface_points, radius=3):
+def get_surface_and_internal_water(water_data, surface_points, radius=3):
     surface_water = []
+    internal_water = []
     for one_water in water_data:
         one_xyz = np.array(one_water[30:54].split()).astype(float)
         dist = find_distances(one_xyz, surface_points)
         check = dist < radius
         if check.any():
             surface_water.append(one_water)
+        else:
+            internal_water.append(one_water)
 
-    return surface_water
+    return surface_water, internal_water
 
 
-def write_surface_water_to_pdb(surface_water_data, output_filename):
+def write_water_to_pdb(water_data, output_filename):
     with open(output_filename, 'w') as pdb:
-        pdb.writelines(surface_water_data)
+        pdb.writelines(water_data)
 
     pass
 
@@ -54,13 +57,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args.filename)
     print(args.surface_file)
-    print(args.surface_water)
+    print(args.output_filename)
     if not args.include_radius:
         args.include_radius = 3
     print(args.include_radius)
     atom_info = read_pdb(args.filename)
     surface_points = read_surface_points(args.surface_file)
     water_data = get_water_data(atom_info)
-    surface_water_data = get_surface_water(water_data, surface_points, args.include_radius)
-    write_surface_water_to_pdb(surface_water_data, args.surface_water)
+    surface_water_data, internal_water_data = get_surface_and_internal_water(water_data,
+                                                        surface_points,
+                                                        args.include_radius)
+    write_water_to_pdb(surface_water_data,
+                       args.output_filename + "_surface.pdb")
+    write_water_to_pdb(internal_water_data,
+                       args.output_filename + "_internal.pdb")
     pass
